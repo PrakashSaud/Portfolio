9e7cfce6e307d9f0796e5b3f04937b638d27c9c9
Prakash Saud <prakashsaud41@gmail.com>
2025-10-01 23:54:17 -0400

    Fix flake8 whitespace issues (final newlines, stray spaces)

--- projects/urls.py
+++ projects/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "projects"
 
 urlpatterns = [
-    path("", views.projects_list, name="list"),              # main list w/ search & filters
+    path("", views.projects_list, name="list"),  # main list w/ search & filters
     path("index/", views.project_index, name="project_index"),  # optional landing page
     path("<slug:slug>/", views.project_detail, name="detail"),
 ]
